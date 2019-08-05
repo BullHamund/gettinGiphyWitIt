d832170f8e8edd70a1cf77155f58fa52b88f2c26
Bill Hammond <willmhammond@gmail.com>
2019-08-05 16:30:00 -0700

    changed background image and topics; fixed javascript to add buttons for topics and successfully make AJax call to giphy api; still cannot get gifs to animate or to alllow user to add topics

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
-$(function () {
-    addButtons(landmarkArray, "searchButton", "#landmark-buttons");
+$(function() {
+    addButtons(memeArray, "searchButton", "#meme-buttons");
     console.log("Page loaded");
-});
+})
     // an array of places in NYC
-    var landmarkArray = ["vuca meme",
+    var memeArray = ["vuca meme",
         "kermit meme",
         "arthur meme",
         "dank meme gifs",
@@ -12,29 +12,62 @@ $(function () {
         "judge judy",
         "like a boss",
         "sips tea",
-        "steal yo girl",
-    ]
+        "steal yo girl"
+    ];
     // function to add and append buttons
-    function addButtons(landmarkArray, classToAdd, areaToAddTo) {
+    function addButtons(memeArray,classToAdd,areaToAddTo) {
         $(areaToAddTo).empty();
-        for(var i = 0; i < landmarkArray.length;i++) {
+        for(var i = 0; i < memeArray.length;i++) {
             var a = $("<button>");
             a.addClass(classToAdd);
-            a.attr("data-type", landmarkArray[i]);
-            a.text(landmarkArray[i]);
+            a.attr("data-type", memeArray[i]);
+            a.text(memeArray[i]);
             $(areaToAddTo).append(a);
         }
     }
 
     $(document).on("click", ".searchButton", function() {
+        $("#gifs-here").empty();
         var type = $(this).data('type');
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=MFCxtiGGppfvi4vmxsmeXWZ82NRKiTzO&limit=10";
         $.ajax({
             url: queryURL,
             method: "GET"
           }).done(function(response) {
-            for(var 1=0; i < response.data.length; i++) {
-                var searchDiv = $("<div class='search'>")
+            for(var i=0; i < response.data.length; i++) {
+                var searchDiv = $("<div class='search-item'>");
+                var rating = response.data[i].rating;
+                var p = $("<p>").text("rating: " + rating);
+                var animated = response.data[i].images.fixed_height.url;
+                var still = response.data[i].images.fixed_height_still.url;
+                var image = $("<img>");
+                image.attr("src", still);
+                image.attr("data-still", still);
+                image.attr("data-animated", animated);
+                image.attr("data-state", "still");
+                image.addClass("searchImage");
+                searchDiv.append(p);
+                searchDiv.append(image);
+                $("#gifs-here").append(searchDiv);
             }
           })
     })
+
+    $(document).on("click", ".searchImage", function() {
+        var state = $(this).data("data-state");
+        if(state == "still") {
+            $(this).attr("src", $(this).data('animated'));
+            $(this).attr("data-state", "animated");
+        } else {
+            $(this).attr("src", $(this).data('still'));
+            $(this).attr("data-state", "still");
+        }
+    })
+
+    $("#addSearch").on("click", function() {
+        var neweSearch = $("input").eq(0).val();
+        memeArray.push(newSearch);
+        addButtons(memeArray, "searchButton", "#meme-buttons");
+        return false;
+    })
+
